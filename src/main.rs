8c7ef5e3db310af3386ac6e40cf752ed12e5cc62
Loic DURAND <loicdurand.ld@gmail.com>
2025-01-31 00:48:00 -0400

    Amélioration des commentaires pour les arguments de catégorie et de tags dans la ligne de commande

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Catégorie à utiliser pour limiter la recherche à un domaine spécifique - ex: "informatique" ou juste "inf", "téléphonie", "radio", etc...
-    #[arg(short, long)]
+    /// Catégorie à utiliser pour limiter la recherche à un domaine spécifique ("informatique", "téléphonie", "radio", etc...)
+    /// - ex: "pwiz -c informatique -f [...]"
+    /// Vous pouvez n'écrire que les premières lettres du terme recherché, par exemple "inf" pour "informatique", "telep" pur "téléphonie"
+    #[arg(short, long,verbatim_doc_comment)]
     categorie: Option<String>,
 
-    /// Flags pour la recherche - ex: "dovecot", "messagerie", "pulsar", etc...
-    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
-    pub flags: Vec<String>,
+    /// Tags pour la recherche - ex: "dovecot", "messagerie", "pulsar", etc...
+    /// Vous pouvez mentionner autant de tags que vous le souhaitez:
+    /// - ex: pwiz -f messagerie dovecot archives
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ',verbatim_doc_comment)]
+    pub tags: Vec<String>,
 }
 
 fn main() {
@@ -19,7 +23,7 @@ fn main() {
         println!("Categorie: {}", categorie);
     };
 
-    for flag in args.flags.iter() {
-        println!("Flag: {}!", flag);
+    for tag in args.tags.iter() {
+        println!("Tag: {}", tag);
     }
 }
