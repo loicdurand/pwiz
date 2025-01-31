85e8027a8b67fc84ece320ca956d9eb1cdd1a7f8
Loic DURAND <loicdurand.ld@gmail.com>
2025-01-31 00:05:22 -0400

    Ajout de la prise en charge des arguments de ligne de commande avec clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Catégorie à utiliser pour limiter la recherche à un domaine spécifique - ex: "informatique" ou juste "inf", "téléphonie", "radio", etc...
+    #[arg(short, long)]
+    categorie: Option<String>,
+
+    /// Flags pour la recherche - ex: "dovecot", "messagerie", "pulsar", etc...
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    pub flags: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for flag in args.flags.iter() {
+        println!("Flag: {}!", flag);
+    }
 }
