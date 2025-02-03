b84986a14a58f6902e58e6a1ef10c7c65837bd96
Loic DURAND <loicdurand.ld@gmail.com>
2025-02-03 00:28:52 -0400

    Ajout de la prise en charge de dotenv pour la gestion des variables d'environnement. Création d'un module fixtures pour insérer des données initiales dans la base de données.

---
+++ src/fixtures.rs
@@ -0,0 +1,68 @@
+use dotenvy::dotenv;
+use std::env;
+
+use polodb_core::{bson::doc, Collection, CollectionT, Database};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tuto {
+    id: i8,
+    content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tag {
+    tuto_id: i8,
+    value: String,
+}
+
+pub fn up() {
+    dotenv().ok(); //charge les variables présente dans le .env dans l'environnement
+
+    let db_path = env::var("DB_PATH") //on tente de récuperer le chemin de la BDD depuis l'environnement
+        .expect("DB_PATH doit etre précisé dans .env"); //si elle n'existe pas on lève une erreur
+
+    let db = Database::open_path(&db_path).unwrap();
+    let tutos = db.collection("tutos");
+    let tags: Collection<Tag> = db.collection("tags");
+
+    tutos
+        .insert_one(Tuto {
+            id: 1,
+            content: String::from("sudo cryptsetup luksFormat  /dev/hdXX"),
+        })
+        .unwrap();
+
+    tags.insert_many(vec![
+        Tag {
+            tuto_id: 1,
+            value: String::from("chiffrer"),
+        },
+        Tag {
+            tuto_id: 1,
+            value: String::from("disque"),
+        },
+        Tag {
+            tuto_id: 1,
+            value: String::from("dur"),
+        },
+        Tag {
+            tuto_id: 1,
+            value: String::from("partition"),
+        },
+    ])
+    .unwrap();
+
+    let tuto = tutos.find_one(doc! {"id":1});
+
+    println!("name: {:?}", tuto);
+
+    // let tuto_tags = tags
+    //     .find(doc! {
+    //       "tuto_id": tuto.id
+    //     })
+    //     .run();
+    // for &tag in tuto_tags.iter() {
+    //     println!("{:#?}", &tag);
+    // }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,54 @@
-use clap::Parser;
-use polodb_core::Database;
-use colored::Colorize;
+use dotenvy::dotenv;
+use std::env;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Catégorie à utiliser pour limiter la recherche à un domaine spécifique ("informatique", "téléphonie", "radio", etc...)
-    /// - ex: "pwiz -c informatique -f [...]"
-    /// Vous pouvez n'écrire que les premières lettres du terme recherché, par exemple "inf" pour "informatique", "telep" pur "téléphonie"
-    #[arg(short, long, verbatim_doc_comment)]
-    categorie: Option<String>,
+use polodb_core::{bson::doc, Collection, CollectionT, Database};
+use serde::{Deserialize, Serialize};
 
-    /// Tags pour la recherche - ex: "dovecot", "messagerie", "pulsar", etc...
-    /// Vous pouvez mentionner autant de tags que vous le souhaitez:
-    /// - ex: pwiz -f messagerie dovecot archives
-    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ',verbatim_doc_comment)]
-    pub tags: Vec<String>,
+//use inquire::Text;
+mod fixtures;
+
+#[derive(Debug, Serialize, Deserialize)]
+
+struct Tuto {
+    id: i8,
+    content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tag {
+    tuto_id: i8,
+    value: String,
 }
 
 fn main() {
-    let args: Args = Args::parse();
-    // let db = Database::open_path("pwiz.db").unwrap();
+    let db = establish_connection();
+    let tutos: Collection<Tuto> = db.collection("tutos");
+    //let tags:Collection<Tag>= db.collection("tags");
+    let tuto = tutos.find_one(doc! {"id":1});
 
-    if let Some(categorie) = args.categorie {
-        println!("Categorie: {}", categorie.blue().bold());
-    };
+    println!("tuto: {:?}", tuto);
 
-    for tag in args.tags.iter() {
-        println!("Tag: {}", tag.green().bold());
+    // let commande = Text::new("Que souhaitez-vous faire?").prompt();
+
+    // match commande {
+    //     Ok(commande) => {}
+    //     Err(_) => println!("An error happened when asking for your name, try again later."),
+    // }
+}
+
+pub fn establish_connection() -> Database {
+    dotenv().ok(); //charge les variables présente dans le .env dans l'environnement
+
+    let db_path = env::var("DB_PATH") //on tente de récuperer le chemin de la BDD depuis l'environnement
+        .expect("DB_PATH doit etre précisé dans .env"); //si elle n'existe pas on lève une erreur
+    let run_migration = env::var("RUN_MIGRATION") //on vérifie s'il faut lancer les fixtures
+        .expect("RUN_MIGRATION doit etre précisé dans .env"); //si elle n'existe pas on lève une erreur
+
+    let db = Database::open_path(&db_path).unwrap();
+
+    if run_migration.parse::<i8>().unwrap() == 1 {
+        fixtures::up();
     }
+
+    return db;
 }
